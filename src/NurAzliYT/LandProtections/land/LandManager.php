7044c39b7c7e3f6972b1c624ae713dd94ab47e9a
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-07-16 17:47:42 +0700

    Update LandManager.php

--- src/NurAzliYT/LandProtections/land/LandManager.php
+++ src/NurAzliYT/LandProtections/land/LandManager.php
@@ -2,68 +2,78 @@
 
 namespace NurAzliYT\LandProtections\land;
 
-use NurAzliYT\LandProtections\Main;
+use pocketmine\player\Player;
 use pocketmine\world\Position;
+use pocketmine\plugin\PluginBase;
 
 class LandManager {
-
-    private Main $plugin;
+    private PluginBase $plugin;
     private array $protectedLands = [];
-    private int $landCounter = 1;
 
-    public function __construct(Main $plugin) {
+    public function __construct(PluginBase $plugin) {
         $this->plugin = $plugin;
+        // Load protected lands from the configuration or database here
+        $this->loadProtectedLands();
     }
 
-    public function protectLand(Position $position, string $owner): void {
-        $size = $this->plugin->getConfig()->getNested("land.protection-size");
-        $landID = $this->landCounter++;
-        $this->protectedLands[$landID] = [
-            "owner" => $owner,
-            "position" => $position,
-            "size" => $size,
-            "invited" => []
-        ];
+    private function loadProtectedLands(): void {
+        $data = $this->plugin->getConfig()->get("protectedLands", []);
+        foreach ($data as $entry) {
+            $position = new Position($entry["x"], $entry["y"], $entry["z"], $this->plugin->getServer()->getWorldManager()->getWorldByName($entry["world"]));
+            $this->protectedLands[$position->asVector3()->__toString()] = [
+                "owner" => $entry["owner"],
+                "invites" => $entry["invites"] ?? []
+            ];
+        }
     }
 
     public function isLandProtected(Position $position): bool {
-        foreach ($this->protectedLands as $land) {
-            if ($this->isWithinLand($position, $land["position"], $land["size"])) {
-                return true;
-            }
-        }
-        return false;
+        return isset($this->protectedLands[$position->asVector3()->__toString()]);
     }
 
     public function isOwner(Position $position, string $playerName): bool {
-        foreach ($this->protectedLands as $land) {
-            if ($this->isWithinLand($position, $land["position"], $land["size"]) && $land["owner"] === $playerName) {
-                return true;
-            }
-        }
-        return false;
+        return $this->protectedLands[$position->asVector3()->__toString()]["owner"] === $playerName;
     }
 
-    public function addInvite(Position $position, string $playerName): void {
-        foreach ($this->protectedLands as &$land) {
-            if ($this->isWithinLand($position, $land["position"], $land["size"])) {
-                $land["invited"][] = $playerName;
-                return;
-            }
+    public function canBuild(Position $position, Player $player): bool {
+        if (!$this->isLandProtected($position)) {
+            return true;
+        }
+
+        $landData = $this->protectedLands[$position->asVector3()->__toString()];
+        return $landData["owner"] === $player->getName() || in_array($player->getName(), $landData["invites"], true);
+    }
+
+    public function protectLand(Position $position, string $owner): void {
+        $this->protectedLands[$position->asVector3()->__toString()] = [
+            "owner" => $owner,
+            "invites" => []
+        ];
+        $this->saveProtectedLands();
+    }
+
+    public function addInvite(Position $position, string $invitee): void {
+        $landData = &$this->protectedLands[$position->asVector3()->__toString()];
+        if (!in_array($invitee, $landData["invites"], true)) {
+            $landData["invites"][] = $invitee;
+            $this->saveProtectedLands();
         }
     }
 
-    public function isInvited(Position $position, string $playerName): bool {
-        foreach ($this->protectedLands as $land) {
-            if ($this->isWithinLand($position, $land["position"], $land["size"]) && in_array($playerName, $land["invited"])) {
-                return true;
-            }
+    private function saveProtectedLands(): void {
+        $data = [];
+        foreach ($this->protectedLands as $positionStr => $landData) {
+            $position = Position::fromString($positionStr, $this->plugin->getServer()->getWorldManager());
+            $data[] = [
+                "x" => $position->getX(),
+                "y" => $position->getY(),
+                "z" => $position->getZ(),
+                "world" => $position->getWorld()->getFolderName(),
+                "owner" => $landData["owner"],
+                "invites" => $landData["invites"]
+            ];
         }
-        return false;
-    }
-
-    private function isWithinLand(Position $pos, Position $landPos, int $size): bool {
-        return abs($pos->getX() - $landPos->getX()) <= $size &&
-               abs($pos->getZ() - $landPos->getZ()) <= $size;
+        $this->plugin->getConfig()->set("protectedLands", $data);
+        $this->plugin->getConfig()->save();
     }
 }
