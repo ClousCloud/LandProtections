1a00f6c2b24b706f9699829813c4b9954d65dd0c
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-07-16 17:41:30 +0700

    Update ProtectCommand.php

--- src/NurAzliYT/LandProtections/commands/ProtectCommand.php
+++ src/NurAzliYT/LandProtections/commands/ProtectCommand.php
@@ -6,9 +6,18 @@ use CortexPE\Commando\BaseCommand;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat;
-use NurAzliYT\LandProtections\Main;
+use NurAzliYT\LandProtections\land\LandManager;
+use cooldogedev\BedrockEconomy\api\type\AsyncAPI;
 
 class ProtectCommand extends BaseCommand {
+    private AsyncAPI $economyAPI;
+    private LandManager $landManager;
+
+    public function __construct($plugin, AsyncAPI $economyAPI, LandManager $landManager) {
+        parent::__construct($plugin, "protect", "Protect your land");
+        $this->economyAPI = $economyAPI;
+        $this->landManager = $landManager;
+    }
 
     protected function prepare(): void {
         $this->setPermission("landprotections.command.protect");
@@ -21,23 +30,22 @@ class ProtectCommand extends BaseCommand {
         }
 
         $player = $sender;
-        $economyAPI = $this->getOwningPlugin()->getEconomyAPI();
         $cost = $this->getOwningPlugin()->getConfig()->getNested("land.protection-cost");
 
-        $economyAPI->getPlayerBalance($player->getName(), function(float $balance) use ($player, $cost) {
+        $this->economyAPI->getPlayerBalance($player->getName(), function(float $balance) use ($player, $cost) {
             if ($balance < $cost) {
                 $player->sendMessage(TextFormat::RED . "You do not have enough money to protect this land");
                 return;
             }
 
-            if ($this->getOwningPlugin()->getLandManager()->isLandProtected($player->getPosition())) {
+            if ($this->landManager->isLandProtected($player->getPosition())) {
                 $player->sendMessage(TextFormat::RED . "This land is already protected");
                 return;
             }
 
-            $this->getOwningPlugin()->getEconomyAPI()->subtractFromPlayerBalance($player->getName(), $cost, function(bool $success) use ($player) {
+            $this->economyAPI->subtractFromPlayerBalance($player->getName(), $cost, function(bool $success) use ($player) {
                 if ($success) {
-                    $this->getOwningPlugin()->getLandManager()->protectLand($player->getPosition(), $player->getName());
+                    $this->landManager->protectLand($player->getPosition(), $player->getName());
                     $player->sendMessage(TextFormat::GREEN . "Land protected successfully!");
                 } else {
                     $player->sendMessage(TextFormat::RED . "Failed to subtract money for land protection");
